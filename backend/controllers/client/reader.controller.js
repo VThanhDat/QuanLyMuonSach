358db094ee03e91e3042bc3cce25eeb951e5a86f
VThanhDat <121113462+VThanhDat@users.noreply.github.com>
2024-11-07 15:34:22 +0700

    Làm thêm chức năng mượn sách ở người đọc

--- backend/controllers/client/reader.controller.js
+++ backend/controllers/client/reader.controller.js
@@ -42,8 +42,164 @@ const getAll = async (req, res) => {
     }
 }
 
+const borrowBook = async (req, res) => {
+    try {
+        const tokenUser = req.cookies.tokenUser;
+        if (tokenUser) {
+            const reader = await Reader.findOne({ token: tokenUser });
+
+            if (!reader) {
+                return res.status(404).json({message: 'Reader not found'})
+            }
+
+            if (!Array.isArray(reader.borrow)) {
+                reader.borrow = [];
+            }
+
+            const newBorrow = {
+                id_book: req.body.borrow.id_book,
+                status: req.body.borrow.status || "processing",
+                borrowDate: req.body.borrow.borrowDate || "01/01/2024",
+                returnDate: req.body.borrow.returnDate || "31/12/2024",
+                quantity: req.body.borrow.quantity || 1,
+            }
+
+            const readers = await Reader.find({})
+            let borrowedBookQuantity = 0;
+
+            readers.forEach(function (reader) {
+                reader.borrow.forEach(function (borrow) {
+                    if (borrow.id_book === req.body.borrow.id_book) {
+                        borrowedBookQuantity += borrow.quantity
+                    }
+                })
+            })
+
+            const book = await Book.findById(req.body.borrow.id_book)
+
+            if (!book) {
+                // Kiểm tra xem sách có tồn tại hay không
+                return res.status(404).json({message: 'Book not found'})
+            }
+
+            if (book.quantity === 0) {
+                // Không còn sách trong kho
+                console.log("Không còn sách trong kho để mượn")
+                return res.status(400).json({message: "Không còn sách trong kho để mượn"})
+            } else if (book.quantity - borrowedBookQuantity - newBorrow.quantity < 0) {
+                // Số lượng sách đã được mượn bằng hoặc vượt quá số lượng sách trong kho
+                console.log("Số lượng sách đã mượn đã đạt tới giới hạn")
+                return res.status(400).json({message: "Số lượng sách đã mượn đã đạt tới giới hạn"})
+            }
+
+            // Kiểm tra xem đã có quyển sách trong mảng borrow chưa
+            const existingBook = reader.borrow.find(book => book.id_book === newBorrow.id_book)
+
+            if (existingBook) {
+                // Nếu đã có quyển sách trong mảng borrow, cập nhật số lượng
+                existingBook.quantity += newBorrow.quantity
+                existingBook.borrowDate = newBorrow.borrowDate || "01/01/2024"
+                existingBook.returnDate = newBorrow.returnDate || "31/12/2024"
+                existingBook.status = newBorrow.status || "processing"
+            } else {
+                // Nếu chưa có quyển sách trong mảng borrow, thêm borrow vào mảng
+                reader.borrow.push(newBorrow)
+            }
+
+            // Lưu lại thay đổi vào CSDL
+            await reader.save()
+            
+            res.status(200).json({message: 'Cập nhật mượn sách thành công', reader})
+        }
+    } catch (error) {
+        console.error('Cập nhật mượn sách thất bại:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+const deleteBookFromBorrow = asyncHandler(async (req, res) => {
+    try {
+        const tokenUser = req.cookies.tokenUser;
+        const id = req.params.id;
+
+        if (tokenUser) {
+            const reader = await Reader.findOne({
+                token: tokenUser,
+            })
+
+            if (reader) {
+                // Lọc ra các danh sách trong borrow mà id_book không bằng id cần xóa
+                reader.borrow = reader.borrow.filter(book => book.id_book !== id)
+
+                // Lưu thay đổi vào CSDL
+                await reader.save()
+                res.status(200).json({message:"Book deleted successfully."})
+            } else {
+                res.status(404).json({message: "Reader not found."})
+            }
+        } else {
+            res.status(401).json({message: "Unauthorized."})
+        }
+    } catch (error) {
+        res.status(500).json({ message: `Error! ${error}` });
+    }
+})
+
+const statusBookReturn = async (req, res) => {
+    try {
+        // Lấy thông tin từ request
+        const { readerId, bookId } = req.params
+        const { status } = req.body
+        //Kiểm tra xem reader và book có tồn tại không
+        const reader = await Reader.findById(readerId)
+        if (!reader) {
+            res.status(404).json({ message: "Reader not found." })
+            return;
+        }
+        const bookIndex = reader.borrow.findIndex(book => book.id_book === bookId)
+        if (bookIndex === -1) {
+            res.status(404).json({ message: "Book not found." })
+            return;
+        }
+        console.log("bookIndex", bookIndex)
+        // Thay đổi trạng thái sách
+        reader.borrow[bookIndex].status = status
+
+        // Lưu thay đổi vào CSDL
+        await reader.save()
+
+        // Trả về thông báo thành công
+        res.status(200).json({message:"Status updated successfully."})
+    } catch (error) {
+        res.status(500).json({ message: `Error! ${error}` });
+    }
+}
+
+const getNumberBookBorrowed = asyncHandler(async (req,res) => {
+    try {
+        const readers = await Reader.find({})
+        let borrowedBookQuantity = 0
+
+        readers.forEach(function (reader) {
+            reader.borrow.forEach(function (borrow) {
+                if (borrow.id_book === req.params.id_book) {
+                    borrowedBookQuantity += borrow.quantity
+                }
+            })
+        })
+
+        res.status(200).json({message: 'Send NumberBookBorrowed successfully',borrowedBookQuantity})
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
 module.exports = {
     create,
     getUser,
-    getAll
+    getAll,
+    deleteBookFromBorrow,
+    borrowBook,
+    statusBookReturn,
+    getNumberBookBorrowed,
 }
